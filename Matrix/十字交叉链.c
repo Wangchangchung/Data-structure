9e16011dfd7bb7ba217e95221f180bbb9a9e95ec
Wangchangchung <1132629383@qq.com>
2016-10-11 16:29:41 +0800

    add Matrix Code

---
+++ Matrix/十字交叉链.c
@@ -0,0 +1,349 @@
+﻿#include<stdio.h>
+#include<malloc.h>
+
+//十字交叉链中的元素节点类型
+typedef struct SM_CR_ELEMENT{
+
+	int value;
+	struct SM_CR_ELEMENT *nextRow;
+	struct SM_CR_ELEMENT *nextCol;
+
+}SM_CR_ELEMENT;
+
+//十字交叉链的左上角的控制头节点
+typedef struct SM_CR_HEADER{
+
+	int rowCount;
+	int colCount;
+	SM_CR_ELEMENT **rowHeadPoint;
+	SM_CR_ELEMENT **colHeadPoint;
+}SM_CR_HEADER;
+
+#define FOUND_ROW    0
+#define FOUND_COL    1
+#define NOT_FOUND   -1
+#define FALSE		 0
+#define TRUE		 1
+#define USER_TYPE    int
+
+
+void initCrossLink(SM_CR_HEADER **head);										//初始化
+
+int getRowOrCol(SM_CR_HEADER head, SM_CR_ELEMENT *node, int maxIndex, int findType);   //得到节点的行值和列值得合并方法
+
+int get_CR_RowIndex(SM_CR_HEADER head, SM_CR_ELEMENT *node);					//得到节点的行值
+
+int get_CR_ColIndex(SM_CR_HEADER head, SM_CR_ELEMENT *node);					//得到节点的列值
+
+void in_Put_One_Element(SM_CR_HEADER head, void (*input)(USER_TYPE *));			//将一个节点保存到十字交叉链中
+
+void init_Element_Value(int *value);											//用户输入给每一个元素赋值
+
+void creat_One_Element(SM_CR_HEADER head, USER_TYPE  value, int row, int col);							//为一个节点申请空间
+
+void insert_Element_Into_CRLink(SM_CR_HEADER head, SM_CR_ELEMENT *newNode, int row, int col);			//将一个节点直接插入到十字交叉链中。
+
+void insert_Element_Into_RowLink(SM_CR_HEADER head, SM_CR_ELEMENT *newNode, int row, int col);			//插入到行
+
+void insert_Element_Into_ColLink(SM_CR_HEADER head, SM_CR_ELEMENT *newNode, int row, int col);			//插入到列
+
+void init_Matrix(SM_CR_HEADER head);											//初始化矩阵
+
+void show_SMCR_Matrix(SM_CR_HEADER head);										//显示矩阵
+
+void destory_Matrix(SM_CR_HEADER **head);										//销毁矩阵
+
+
+void destory_Matrix(SM_CR_HEADER **head){
+
+	if((*head)==NULL){
+		return ;
+	}
+
+	//释放行列的控制头指针。
+	if((*head)->colHeadPoint && (*head)->rowHeadPoint){
+		free((*head)->colHeadPoint);
+		free((*head)->rowHeadPoint);
+	}
+	//释放十字交叉链的控制头
+	free(*head);
+	*head = NULL;
+}
+
+
+void show_SMCR_Matrix(SM_CR_HEADER head){
+
+	int col;
+	int row;
+	SM_CR_ELEMENT *node = NULL;
+
+	printf("\n");
+	for(row = 0; row < head.rowCount; row++){
+		//先得到每一行的头指针
+		node = head.rowHeadPoint[row];
+		for(col = 0; col < head.colCount; col++){
+			//根据得到node的列下标值  与循环col的比较，当遇到存储元素的节点的时候就输出
+			if(get_CR_ColIndex(head, node) == col){
+				printf("%d\t", node->value);
+				node = node->nextCol;			//同时 node指向下一个节点,虽然col循环一直在判断，但是没有值得位置与col是
+			}else{
+				printf("0\t");
+			}
+			//注意，不能再这里写  node = node->nextCol;
+		}
+		printf("\n");
+	}
+
+}
+
+void init_Matrix(SM_CR_HEADER head){
+	int count;
+	int i;
+
+	printf("请输入有效元素个数:");
+	scanf("%d", &count);
+
+	for(i = 0 ; i < count; i++){
+		in_Put_One_Element(head, init_Element_Value);
+	}
+}
+
+void insert_Element_Into_ColLink(SM_CR_HEADER head, SM_CR_ELEMENT *newNode, int row, int col){
+
+	
+	SM_CR_ELEMENT *preElement =NULL;		//用于保存前一个节点的指针(地址)
+	
+	SM_CR_ELEMENT *curElement = NULL;		//用于保存当前节点的指针(地址)
+
+	//如果该列中没有元素， 或者第一个插入的位置是原有列元素的最前面，那么直接更改head.colHeadPoint[col] 的指向
+	//将元素进行插入
+	if(head.colHeadPoint[col] == NULL || get_CR_RowIndex(head, head.colHeadPoint[col]) > row ){
+		newNode->nextRow = head.colHeadPoint[col];
+		head.colHeadPoint[col] = newNode;
+	}else{
+		
+		//插入已有元素的中间 或者是在末尾插入。
+		//当是要在列的末尾插入的时候最后 curElement = curElement->nextRow = null; 这个的get_CR_RowIndex()返回的是NOT_FOUND
+		//调出循环,while循环的后面是为了保证中间的插入的情况。
+		preElement = head.colHeadPoint[col];
+		curElement = preElement;
+		while(get_CR_RowIndex(head, curElement)!= NOT_FOUND && get_CR_RowIndex(head, curElement) < row){
+			preElement = curElement;
+			curElement = curElement->nextRow;
+		}
+		//将节点链接
+		newNode->nextRow = preElement->nextRow;
+		preElement->nextRow = newNode;
+	}
+
+}
+
+void insert_Element_Into_RowLink(SM_CR_HEADER head, SM_CR_ELEMENT *newNode, int row, int col){
+
+
+	//找的方法上面的方法是一样的。 注意找Row的时候用的方法不要搞混了。
+	SM_CR_ELEMENT *preElement =NULL;
+	SM_CR_ELEMENT *curElement = NULL;
+
+
+	if(head.rowHeadPoint[row] == NULL || get_CR_ColIndex(head, head.colHeadPoint[row]) > col){
+		newNode->nextCol= head.rowHeadPoint[row];
+		head.rowHeadPoint[row] = newNode;
+	}else{
+		
+		preElement = head.rowHeadPoint[row];
+		curElement = preElement;
+		while(get_CR_ColIndex(head, curElement)!= NOT_FOUND && get_CR_ColIndex(head, curElement) < col){
+			preElement = curElement;
+			curElement = curElement->nextCol;
+		}
+		newNode->nextCol = preElement->nextCol;
+		preElement->nextCol= newNode;
+
+	}
+
+}
+
+void insert_Element_Into_CRLink(SM_CR_HEADER head, SM_CR_ELEMENT *node, int row, int col){
+	//插入行
+	insert_Element_Into_RowLink(head, node, row,  col);
+	//插入列
+	insert_Element_Into_ColLink(head, node, row,  col);
+	printf("插入一个节点成功！");
+
+}
+
+void creat_One_Element(SM_CR_HEADER head, USER_TYPE  value, int row, int col){
+
+	SM_CR_ELEMENT *newElement = NULL;
+
+	newElement = (SM_CR_ELEMENT *)calloc(sizeof(SM_CR_ELEMENT), 1);
+	newElement->value = value;
+	newElement->nextRow = NULL;
+	newElement->nextCol = NULL;
+	printf("创建一个节点成功！");
+	//链入进十字交叉链中
+	insert_Element_Into_CRLink(head, newElement, row, col);
+
+}
+
+void init_Element_Value(int *value){
+
+	printf("请输入节点的值:");
+	scanf("%d", value);
+
+}
+
+void in_Put_One_Element(SM_CR_HEADER head, void (*input)(USER_TYPE *)){
+
+	USER_TYPE value;
+	int row;
+	int col;
+
+	//调用我在外面定义的方法完成输入一个用户类型的数值。
+	input(&value);
+	printf("请输入该节点的行值,列值,列如(3 4):");
+	scanf("%d %d", &row, &col);
+
+	//调用创建一个节点的方法
+	creat_One_Element(head, value, row, col);
+
+}
+
+
+
+int get_CR_ColIndex(SM_CR_HEADER head, SM_CR_ELEMENT *node){
+	int colIndex;
+	SM_CR_ELEMENT *colPoint=NULL;
+	
+	if(!node){
+		return NOT_FOUND;
+	}
+
+	//遍历每一列列
+	for(colIndex = 0; colIndex < head.colCount; colIndex++){
+		for(colPoint = head.colHeadPoint[colIndex]; colPoint; colPoint=colPoint->nextRow){
+			if(colPoint == node){
+				return colIndex;
+			}
+		}
+	}
+	return NOT_FOUND;
+}
+	
+int get_CR_RowIndex(SM_CR_HEADER head, SM_CR_ELEMENT *node){
+	int rowIndex = 0;
+	SM_CR_ELEMENT *rowPoint=NULL;
+	
+	if(!node){
+		return NOT_FOUND;
+	}
+
+	for(rowIndex =0; rowIndex < head.rowCount; rowIndex++){
+		for(rowPoint = head.rowHeadPoint[rowIndex]; rowPoint; rowPoint= rowPoint->nextCol){
+			if(rowPoint == node){
+				return rowIndex;
+			}	
+		}
+	}
+	return NOT_FOUND;
+}
+
+
+int getRowOrCol(SM_CR_HEADER head, SM_CR_ELEMENT *node, int maxIndex, int findType){
+
+	SM_CR_ELEMENT *p = NULL;
+	int result  = NOT_FOUND;
+	int index = 0;
+	unsigned char  found =  FALSE;
+	
+	//没有找到，没有找完
+	while(!found &&  index < maxIndex){
+		
+		//如果是找的类型是col 就选择colHeadPoint 列的头指针
+		p = (findType == FOUND_COL ? head.colHeadPoint : head.rowHeadPoint)[index];
+
+		//如果这个链里没有元素就不找了
+		//这里循环的条件还有就是没有
+		while(p && !found){
+			if(p== node){
+				found = TRUE;
+			}else{
+				//往下面找
+				p = (findType  == FOUND_COL ? p->nextRow : p->nextCol);
+			}
+		}
+		//判断在该列/行中是否找到对应的下标值了，如果没找到就找下一列或行。
+		if(!found){
+			index++;
+		}
+	}
+		
+	result = index;
+	return result;
+
+	/*
+	一般的做法是通过下面的方式:分别来找到行列的值，但是都是可以提取到上面的函数。
+	if(findType == FOUND_COL){
+		
+		index = 0;
+		while(!found && index < maxIndex){
+		
+			while(p && !found){
+			
+				if(p == node){
+					found = TRUE;
+				}else{
+					p = p->nextRow;
+				}
+			}
+
+			if(!found){
+				index++;
+			}
+		}
+	}
+	result = index;
+	return result;
+	*/
+}
+
+void initCrossLink(SM_CR_HEADER **head){
+
+	SM_CR_HEADER  *p = NULL;
+	
+	if(NULL == (*head)){
+		
+		int colCount;
+		int rowCount;
+
+		p = (SM_CR_HEADER *)calloc(sizeof(SM_CR_HEADER), 1);
+
+		printf("请输入矩阵的行数和列数,如(3 5):");
+		scanf("%d %d", &rowCount, &colCount);
+		
+		p->rowCount = rowCount;
+		p->colCount = colCount;
+
+		//申请行和列值针空间，  每个元素都是指针
+		p->rowHeadPoint = (SM_CR_ELEMENT **)calloc(sizeof(SM_CR_ELEMENT *), rowCount);
+		p->colHeadPoint = (SM_CR_ELEMENT **)calloc(sizeof(SM_CR_ELEMENT *), colCount);
+		(*head) = p;
+	}
+}
+
+void main(void){
+
+	SM_CR_HEADER *head = NULL;
+	SM_CR_HEADER *head1 = NULL;
+	
+	//初始化十字交叉链的控制头和 列和行的指针
+	initCrossLink(&head);
+	//初始化矩阵(节点)
+	init_Matrix(*head);
+	//显示矩阵
+	show_SMCR_Matrix(*head);
+	//销毁矩阵
+	destory_Matrix(&head);
+
+}
---
+++ Matrix/稀疏矩阵三元组.c
@@ -0,0 +1,205 @@
+﻿#include<stdio.h>
+#include<malloc.h>
+
+typedef struct SM_ELEMENT{
+
+	int row;
+	int col;
+	int value;
+
+}SM_ELEMENT;
+
+typedef struct SM_HEADER{
+	
+	SM_ELEMENT *elememts;
+	int rowCount;
+	int colCount;
+	int elememtCount;
+
+}SM_HEADER;
+
+//
+
+void initSparseMatrix(SM_HEADER **head);											   //初始化三元组矩阵
+
+SM_HEADER *initSparseMatrixHead(int rowCount, int colCount, int elementCount);         //初始化三元组矩阵的控制头
+
+void destory_Sparse_Matrix_Head(SM_HEADER *head);									   //销毁矩阵
+	
+void show_Sparse_Matrix(SM_HEADER head);											   //显示矩阵
+
+SM_HEADER *revange_Sparse_Matrix(SM_HEADER head);
+
+SM_HEADER *revange_Sparse_Matrix(SM_HEADER head){
+
+	SM_HEADER * revangeSparse = NULL;
+	int *postion;
+	int i;
+	int index;
+
+
+	//为转置之后的矩阵申请一个空间
+	revangeSparse = initSparseMatrixHead(head.colCount, head.rowCount, head.elememtCount);
+	
+	
+	/*revangeSparse = (SM_HEADER *)calloc(sizeof(SM_HEADER),1);
+	revangeSparse->rowCount = head.colCount;
+	revangeSparse->colCount = head.rowCount;
+	revangeSparse->elememts = (SM_ELEMENT *)calloc(sizeof(SM_ELEMENT),head.elememtCount);
+	*/
+
+	//为数组申请空间
+	postion = (int *)calloc(head.colCount + 1,sizeof(int));
+
+
+	/*
+		下面代码的具体思路分析，请看该文件下的三元组矩阵.bmp文件。
+	*/
+
+	
+	/*
+	for(i = 0; i < revangeSparse->elememts; i++){
+		postion[head.elememts[i].col + 1]++;
+	}*/
+	
+	//用数组统计每个元素出现在有效元素的次数，保存在col+ 1的下标下，具体为什么，请看解析图
+	//这个位置有"东西"通知下面的要让出位置来。
+	for(i = 0; i < head.elememtCount; i++){
+		postion[head.elememts[i].col + 1]++;
+	}
+
+	for(i = 1; i <= head.colCount ; i++){
+		//求和，因为后面要看当前的元素前面有多少个，才好直接进行过位置的插入。
+		postion[i] += postion[i-1];
+	}
+
+	for(i = 0; i < head.elememtCount; i++){
+		index = postion[head.elememts[i].col];
+		revangeSparse->elememts[index].row = head.elememts[i].col;
+		revangeSparse->elememts[index].col = head.elememts[i].row;
+		revangeSparse->elememts[index].value = head.elememts[i].value;
+
+		postion[head.elememts[i].col]++;
+		//这里可不是postion[index+1]++;   就是因为这个的原因，使你找了半天的问题
+	}
+	free(postion);
+
+	return revangeSparse;
+
+}
+
+
+
+void show_Sparse_Matrix(SM_HEADER head){
+
+	int i;
+	int j;
+	
+	int index = 0;		//记录在用于存储在有效元素数组中的下标
+
+	printf("\n");
+	
+	for(i = 0; i < head.rowCount; i++){		//循环遍历矩阵 行和列
+		for(j = 0; j < head.colCount; j++){
+			
+			if(i == head.elememts[index].row && j == head.elememts[index].col){//当遍历到和有效元素中的保存的行和列的值相同的时候，就是找到一个有效的元素
+				
+				printf("%d\t", head.elememts[index++].value);//输出有效个数，同时index++
+			}else{
+				printf("0\t");                               //不是有效元素的输出0
+			}
+		}
+		printf("\n");
+	}
+
+}
+
+void destory_Sparse_Matrix_Head(SM_HEADER *head){
+	
+	if(head){							//不能释放一个没有初始化矩阵的头结点
+		if(head->elememts){			//释放存储有效节点元素的空间
+			free(head->elememts);
+		}
+		free(head);					//释放控制头的空间。
+	}
+	
+}
+
+SM_HEADER *initSparseMatrixHead(int rowCount, int colCount, int elementCount){
+
+	SM_HEADER *head = NULL;
+
+	//申请控制头空间， 并初始化
+	head = (SM_HEADER *)malloc(sizeof(SM_HEADER));
+	head->rowCount = rowCount;
+	head->colCount = colCount;
+	head->elememtCount = elementCount;
+	//申请有效节点的空间
+	head->elememts = (SM_ELEMENT *)malloc(sizeof(SM_ELEMENT)*elementCount);
+
+	//返回指针。
+	return head;
+}
+
+void initSparseMatrix(SM_HEADER **head){
+
+	/*
+		1、确定矩阵行数，列数。
+	*/
+	int rowCount;
+	int colCount;
+	int elememtCount;
+	int i;
+	int row;
+	int col;
+	int value;
+
+
+	if(NULL == *head){
+		printf("请输入矩阵的行数，列数及有效元素个数,如(2 3 4):\n");
+		scanf("%d %d %d", &rowCount, &colCount, &elememtCount);
+
+		(*head) = initSparseMatrixHead(rowCount, colCount, elememtCount);
+
+		if(*head){
+			printf("请输入%d个有效元素的行值，列值，数据值,如(1 2 89):\n", elememtCount);
+
+			//用户进行输入有效元素。
+			for(i = 0; i < elememtCount; i++){
+				printf("请输入第%d个元素:",i + 1);
+				scanf("%d %d %d", &row, &col, &value);
+				
+				//这里本来因该对输入的韩烈值进行有效性的验证。
+				if(row < rowCount && row >= 0 && col < colCount && col >= 0){
+					(*head)->elememts[i].row = row;
+					(*head)->elememts[i].col = col;
+					(*head)->elememts[i].value = value;
+				}else{
+					printf("你输入的行列值超出你所设定的行列值！");
+					return ;
+				}
+			}
+		}
+	}
+}
+
+void main(void){
+
+	SM_HEADER * head = NULL;
+	SM_HEADER * head1 = NULL;
+	//初始化矩阵
+	initSparseMatrix(&head);
+	
+	//显示矩阵
+	show_Sparse_Matrix(*head);
+
+	head1 = revange_Sparse_Matrix(*head);
+
+	printf("矩阵转置后:\n");
+	show_Sparse_Matrix(*head1);
+	
+	//销毁矩阵
+	destory_Sparse_Matrix_Head(head);
+	destory_Sparse_Matrix_Head(head1);
+
+}
