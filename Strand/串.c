a745c69e9ad82ef5df43ecf96ce41053ee9b15ba
Wangchangchung <1132629383@qq.com>
2016-12-29 11:02:40 +0800

    add Strand

---
+++ Strand/串.c
@@ -0,0 +1,132 @@
+﻿#include<stdio.h>
+#include<malloc.h>
+
+typedef struct STRING_UNIT{
+	char strings[16];
+	struct SRTING_UNIT *next;
+	char len;
+
+}STRING_UNIT;
+
+
+typedef unsigned char boolean;
+
+#define TRUE				1
+#define FALSE				0
+#define NOT_FOUND_INDEX		-1
+
+
+typedef struct STRING{
+	char * string;
+	int stringLength;
+	int MaxLength;
+	
+}STRING;
+
+   
+
+
+boolean init_String(STRING **str, int MaxStringLength);
+
+boolean destory_String(STRING **str);
+
+int get_String_Length(STRING str);
+
+boolean is_String_Full(STRING str);
+
+boolean is_String_Empty(STRING str);
+
+STRING * string_Copy(STRING *target_String, const STRING resource);
+
+STRING * string_Copy(STRING *target_String, const STRING resource){
+
+	int i ;
+
+		//原串和目的串的复制，这里要考虑的问题就有些多了。
+		//如果原串的长度比目标串的长度更长，那么目标串的大小要重新申请
+	if(target_String == NULL){
+		//初始化target_string  申请它的空间长度为 resource大小的空幻
+		//申请一块控制头 再申请string空间  但是这里的操作还没对s1的指向没有更改 ，、
+		//更改的只是target_String的指向。
+		init_String( &target_String, resource.MaxLength);
+	}else if(target_String->MaxLength< resource.MaxLength){
+		/*
+			STRING *s1 = NULL;
+			STRING *s2 = NULL;
+
+		  ini_String(&s1, 12);
+		  ini_String(&s2, 22);
+		  ...
+		  string_Copy(s1, *s2);
+
+		  在 string_Copy()函数中如果进行下面的操作的话
+		  没有在外部正真的更改s1的指向空间的，target_string是一个复制本局部变量
+		  ，在通过target_string s释放空间之后 ，s1指向的是一个释放的空间了。，所以
+		  这种方案是错误的，有两种解决方案
+		  1、将destory_Copy()中的参数更改为二级指针，直接修该s1的指向
+		  2、将s1中的存储string的空间重新申请，s1的指向没有改变
+
+		  这里建议采用第二种个方法
+		*/
+
+		//销毁原来的申请的string空间, 重新申请
+		free(target_String->string);
+		target_String->string = (char *)malloc(sizeof(char)*resource.MaxLength);
+		target_String->MaxLength = resource.MaxLength;
+		//target_String->stringLength = resource.stringLength;
+		//上面这句话不要放到这里，放在在下面的话就可以减少一句代码了。
+
+	}
+
+	//将元素拷过去
+	for(i = 0; i < resource.stringLength;++i){
+		target_String->string[i] = resource.string[i];
+	}
+	//将字符串的长度重新复制
+	target_String->stringLength = resource.stringLength;
+	return target_String;
+}
+
+boolean is_String_Empty(STRING str){
+	return str.stringLength <= 0;
+}
+
+boolean is_String_Full(STRING str){
+	return str.MaxLength <= str.stringLength;
+}
+
+int get_String_Length(STRING str){
+	return str.stringLength;
+}
+
+boolean destory_Stirng(STRING **str){
+	
+	//判断是否为已经初始化的串
+	if(*str){
+		//如果string中有内存空间的使用就先释放string的空间。
+		if((*str)->string)
+			free((*str)->string);
+		free(*str);
+		return TRUE;
+	}
+	return FALSE;
+}
+
+boolean init_String(STRING **str, int MaxStringLength){
+
+	//判断是否是已经初始化过的串，没有进过初始化的串才能进行初始化。
+	if(*str){
+		return FALSE;
+	}
+	*str = (STRING *)malloc(sizeof(STRING));
+	(*str)->string = (char *)malloc(sizeof(char)*MaxStringLength);
+	(*str)->stringLength = 0;
+	(*str)->MaxLength = MaxStringLength;
+	return TRUE;
+}
+
+void main(void)
+{
+
+
+}
---
+++ Strand/字符串匹配算法.c
@@ -0,0 +1,67 @@
+﻿#include<stdio.h>
+#include<string.h>
+
+/*
+	查找子串在主串中出现的位置。
+	1、BF算法(最常规的方法、最暴力的方法、最笨的方法)
+		主串遍历一遍。从第一个开始 然后一个一个的子串进行比较
+		向后面，如果匹配失败，那么就主串的下一个继续继续和子串一个一个的比较
+		知道找到匹配的位置，但是这里找到的是第一次出现的地方(程序直接就返回了)
+		，后面的没有再进行查找，如果想得打从主串的第几个到主串的第几个是子串的位置
+		那么找到后面的后，就直接减-子串长度就可以。
+	时间复杂度： O(m*n) 
+	空间复杂度   O(1);
+
+	
+	2、KMP算法
+
+
+
+*/
+	
+					
+int string_Matching_BF(char *mainString, const char * childString);	//串匹配： 第一个字符串主串， 第二个 子串
+
+int string_Matching_BF(char *mainString, const char * childString){
+	int mainLength;
+	int childLength;
+	int mainIndex = 0;
+	int childIndex = 0;
+
+	mainLength = strlen(mainString);	//主串长度
+	childLength =strlen(childString);	//子串长度
+
+	//遍历主串	
+	while(mainIndex <= mainLength){
+		childIndex = 0;
+		//依次和子串进行比较
+		while(childString[childIndex++] == mainString[mainIndex++]){
+			//childIndex == childLength  说明这个是匹配成功了，返回最后的位置
+			if(childIndex == childLength){
+				return mainIndex -1 ;
+			}
+			//这个大于子串的长度，说明是没有匹配成功，这一步是在
+			/*if(childIndex > childLength){
+				printf("你是子啊if里传来的");
+				return -1;	
+			}*/
+		}
+		//++mainIndex; 这里不用再 加了 因为mainString[mainIndex++] 中 已经加了。
+	}
+	printf("你是从后里传来的");
+	return -1;
+	
+}
+
+void main(void)
+{
+	char * str = "asdasdfafdasfasdffs";
+	char * hel = "dasdfafd";
+
+	int index;
+
+	//index = string_Matching(hel, str);
+	 index = string_Matching_BF(str,hel);
+	printf("%d\n", index);
+
+}
